17c6b867ec28067f50823b8ab87ac78e97f426b7
samayagrawal <samayagrawal.dev@gmail.com>
2026-03-16 19:56:59 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          Samay Codes
+          Scene Maker
         </a>
         <a
           href="mailto:samayagrawal.dev@gmail.com"
